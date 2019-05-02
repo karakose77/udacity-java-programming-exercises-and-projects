28bcaa38dfae9f63882ed3102846f9094bd35f6a
Mustafa Karaköse <karakose77@gmail.com>
2019-05-02 15:37:26 +0300

    initial commit

--- mk001-CountBlocks/CountBlocks.java
+++ mk001-CountBlocks/CountBlocks.java
@@ -1,7 +1,7 @@
 public class CountBlocks {    
     /**
-     * Returns total number of blocks required to build a pyramide, for given number
-     * of pyramide levels.
+     * Returns total number of blocks required to build a pyramide, 
+     * for given number of pyramide levels.
      * <p>
      * @param levels: Number of pyramide levels.
      * @return blockCount: Total number of blocks required to build the pyramide.
